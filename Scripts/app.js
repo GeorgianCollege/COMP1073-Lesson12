2ddc0d56c9263821a475877ec1b6b1a060efb1d2
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-04-07 10:18:37 -0400

    added stage object

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,21 +6,45 @@
     "use strict";
 
     // Load the navbar dynamically via jQuery and Ajax
-    var header = $('#mainHeader').load("partials/nav.html", function(){
+    var header = $('#mainHeader').load("partials/nav.html", function() {
         var bodyid = $('body').attr('id');
         switch (bodyid.toString()) {
             case "index":
-                $('#indexLink').attr("class","active");
+                $('#indexLink').attr("class", "active");
                 break;
             case "projects":
-                $('#projectsLink').attr("class","active");
+                $('#projectsLink').attr("class", "active");
                 break;
             case "contact":
-                $('#contactLink').attr("class","active");
+                $('#contactLink').attr("class", "active");
                 break;
         }
     });
 
+    //CreateJS Section ++++++++++++++++++++++++++++++++++++
+    var stage;
+    var canvas;
     
+    function init() {
+        console.log("Initialization");
+        // reference to canvas element
+        canvas = document.getElementById("canvas");
+        // create a stage container object
+        stage = new createjs.Stage(canvas);
+        // set frame rate to 60 fps
+        createjs.Ticker.setFPS(60);
+        // listen for frame changes and call the animationLoop function
+        createjs.Ticker.addEventListener("tick", animationLoop);
+    }
+    
+    // runs every frame
+    function animationLoop() {
+        // refresh the stage object
+        stage.update();
+    }
+    
+    window.onload = init;
+
+
 })();
 
